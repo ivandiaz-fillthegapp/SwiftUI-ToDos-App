f109f46b5f739cccda2790d5b2aa880bf8520df7
ivandiaz-fillthegapp <94384055+ivandiaz-fillthegapp@users.noreply.github.com>
2026-03-17 11:45:53 +0100

    PX-1: Create main app entry point with LoginScreen as root

---
+++ SwiftUI_ToDos_AppApp.swift
@@ -0,0 +1,12 @@
+import SwiftUI
+
+@main
+struct SwiftUI_ToDos_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                LoginScreen()
+            }
+        }
+    }
+}
